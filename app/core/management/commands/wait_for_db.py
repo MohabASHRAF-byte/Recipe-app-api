3cd498a6d5eb71e20daa975adcc48a390b796679
Mohab Ashraf <a.mohab148@gmail.com>
2024-12-13 21:14:56 +0200

    fixed 2.0

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,7 @@
 """
 Django command to wait for the database to be available
 """
-from django.core.mangement.base import BaseCommnad
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
